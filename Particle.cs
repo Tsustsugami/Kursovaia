c71729bf5648b2df19614c1b347339d35213ed6e
Aleksandr <prevoskhodstvo@list.ru>
2023-04-09 10:49:54 +0800

    Добавьте файлы проекта.

---
+++ Particle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Kursovaia
+{
+    public class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+        public float Life;
+        public static Random rand = new Random();
+        public Particle()
+        {
+            // я не трогаю координаты X, Y потому что хочу, чтобы все частицы возникали из одного места
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
+        }
+        public void Draw(Graphics g)
+        {
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+            var color = Color.FromArgb(alpha, Color.Black);
+            // создали кисть для рисования
+            var b = new SolidBrush(color);
+
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+            // но документация рекомендует делать это самому
+            b.Dispose();
+        }
+    }
+}
